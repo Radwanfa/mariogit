dac7a71c00b3d345180176347cbf74a58ad063ed
Radwanfa <radwanfa068@gmail.com>
2022-07-03 10:36:45 +0200

    full window

--- src/main/java/package1/Window.java
+++ src/main/java/package1/Window.java
@@ -2,12 +2,16 @@ package package1;
 
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
 
 import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
 
 public class Window {
     private int width, height;
     private String title;
+    private long glfwWindow;
 
     private static Window window = null;
 
@@ -48,9 +52,36 @@ public class Window {
         glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
 
         // create window
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if (glfwWindow == NULL) {
+            throw new IllegalStateException("failed to create the window");
+        }
+
+        // make the opengl context current
+        glfwMakeContextCurrent(glfwWindow);
+        // enable v-sync
+        glfwSwapInterval(1);
+
+        // make window visible
+        glfwShowWindow(glfwWindow);
+
+        // This line is critical for LWJGL's interoperation with GLFW's
+        // OpenGL context, or any context that is managed externally.
+        // LWJGL detects the context that is current in the current thread,
+        // creates the GLCapabilities instance and makes the OpenGL
+        // bindings available for use.
+        GL.createCapabilities();
     }
 
     public void loop() {
+        while (!glfwWindowShouldClose(glfwWindow)) {
+            // poll events
+            glfwPollEvents();
 
+            glClearColor(1.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            glfwSwapBuffers(glfwWindow);
+        }
     }
 }
